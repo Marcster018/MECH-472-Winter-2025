762835501a0da313ed29748e08b67c8d8923d8b9
makestuff17 <jacobquilliams@hotmail.com>
2025-04-23 14:48:26 -0400

    Updating Vision and Simulator
    Updated to Gordon's V3.77 and added image_view

--- MECH472 Simulator/vision_simulation.h
+++ MECH472 Simulator/vision_simulation.h
@@ -1,6 +1,10 @@
 
+// maximum number of robots in the simulation
 const int N_MAX = 100;
-	
+
+// maximum string size for obstacle file names
+const int S_MAX = 50;
+
 class robot_system {
 	public:
 
@@ -20,17 +24,11 @@ class robot_system {
 	int N_obs;
 	double x_obs[N_MAX];
 	double y_obs[N_MAX];
-	double size_obs[N_MAX];
 
-	// lighting condition parameters (TODO: add shadow effect, etc.)
-	double light;
-	double light_gradient;
-	double light_dir;
-	double image_noise;
-
-	// 1 player, 2 player, practice, strategies, speed / difficulty level
+	// mode = 0 - single player mode (manual opponent)
+	// mode = 1 - two player mode, player #1
+	// mode = 2 - two player mode, player #2
 	int mode;
-	int level;
 
 	// constructor -- set default simulation parameters
 	robot_system(double D, double Lx, double Ly, 
@@ -43,31 +41,32 @@ class robot_system {
 
 
 int activate_simulation(double width, double height,
-	double x_obs[], double y_obs[], double size_obs[], int N_obs,
+	double x_obs[], double y_obs[], int N_obs,
 	char robot_file[], char opponent_file[], char background_file[],
-	char obstacle_file[], double D, double Lx, double Ly, 
+	char obstacle_file[][S_MAX], double D, double Lx, double Ly, 
 	double Ax, double Ay, double alpha_max, int n_robot);
 	
 int deactivate_simulation();
 
 int set_inputs(int pw_l, int pw_r, int pw_laser, int laser, 
-	double light, double light_gradient, double light_dir,
-	double image_noise, double max_speed, double opponent_max_speed);
+				double max_speed);
 	
 int set_opponent_inputs(int pw_l, int pw_r, int pw_laser, int laser, 
 				double max_speed);	
 
 int acquire_image_sim(image &rgb);
 
-int set_simulation_mode(int mode, int level);
+int set_simulation_mode(int mode);
 
 int set_robot_position(double x, double y, double theta);
 
 int set_opponent_position(double x, double y, double theta);
 
-int rotate(image &a, image &b, image &c, double theta, int ig, int jg);
-	
-int append(image &a, image &b, int ip, int jp);
+int draw_image(image &a, double theta, double ia, double ja, 
+			   image &b, double ib, double jb, image &output,
+			   int blend = 0);
+
+int get_image(image &a, image &b, double ib, double jb, double theta);
 
 int draw_laser(robot *P, image &rgb);
 
