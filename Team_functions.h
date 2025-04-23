d583fe8c53a8dbcd1d100c9b7848b739e5de90e5
SexyL1zard <anthony.garofalo12@gmail.com>
2025-04-23 15:17:40 -0400

    Update Team_functions.h

--- Team_functions.h
+++ Team_functions.h
@@ -3,6 +3,7 @@
 
 //Anthony functions
 void find_hollow_circles(int& nlabels, image& rgb, image& label, image& a, image& rgb0, int Ic[4], int Jc[4])
+void opponent_track(int Ic[4], int Jc[4], int Oc[2], image &rgb, int &pw_r, int &pw_l) 
 
 //Fred functions
 int auto_select_shape_by_size(i2byte& nlabel, image& label)
