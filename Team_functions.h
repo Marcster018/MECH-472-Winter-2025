0806943798e409553bf46387be0040d710138db0
SexyL1zard <anthony.garofalo12@gmail.com>
2025-04-24 12:49:48 -0400

    Update Team_functions.h

--- Team_functions.h
+++ Team_functions.h
@@ -6,7 +6,8 @@ using namespace std;
 
 //Anthony functions
 void find_hollow_circles(int& nlabels, image& rgb, image& label, image& a, image& rgb0, int Ic[4], int Jc[4])
-void opponent_track(int Ic[4], int Jc[4], int Oc[2], image &rgb, int &pw_r, int &pw_l) 
+void opponent_track(int Ic[4], int Jc[4], int Oc[2], image &rgb, int &pw_r, int &pw_l)
+void find_obstacles(image& rgb, image& label, image&a,int nlabel,int Obs[2][2])
 
 //Fred functions
 int auto_select_shape_by_size(i2byte& nlabel, image& label)
