e714799a2106c0681363395ddcf3c9317f85e166
SexyL1zard <anthony.garofalo12@gmail.com>
2025-04-28 15:31:40 -0400

    Update Team_functions.cpp

--- Team_functions.cpp
+++ Team_functions.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 //Anthony functions
-void find_hollow_circles(int& nlabels, image& rgb, image& label, image& a, image& rgb0, int Ic[4], int Jc[4]) {
+void find_hollow_circles(int& nlabels, image& rgb, image& label, image&a, image& rgb0, int Ic[4], int Jc[4]) {
 	ibyte* p, * pc;
 	i2byte* pl, * plc;
 
@@ -69,26 +69,132 @@ void find_hollow_circles(int& nlabels, image& rgb, image& label, image& a, image
 
 }
 
-void opponent_track(int Ic[4], int Jc[4], int Oc[2], image &rgb, int &pw_r, int &pw_l) {
+void clean_up(image& rgb, image&a) {
+	int width, height;
+	image b;
+
+	width = rgb.width;
+	height = rgb.height;
+
+	b.type = GREY_IMAGE;
+	b.width = width;
+	b.height = height;
+
+	allocate_image(b);
+
+	copy(rgb, a);
+	threshold(a, b, 70);
+	copy(b, a);
+	lowpass_filter(a, b);
+	copy(b, a);
+	invert(a, b);
+	copy(b, a);
+	erode(a, b);
+	copy(b, a);
+	dialate(a, b);
+	copy(b, a);
+	dialate(a, b);
+	copy(b, a);
+	copy(a, rgb);
+
+	free_image(b);
+}
+
+void find_obstacles(image& rgb, image& label, image& a, int nlabel, int ObsLabel[2]) {
+	int i, j, k, l, height, width, size;
+	double ic, jc;
+	i2byte* pl;
+
+	pl = (i2byte*)label.pdata;
+	height = rgb.height;
+	width = rgb.width;
+	size = width * height;
+
+	int dx[8] = { 1, 1, 0, -1, -1, -1, 0, 1 };
+	int dy[8] = { 0, -1, -1, -1, 0, 1, 1, 1 };
 	
-	int i = (Ic[3] + Ic[2])/2;
-	int j = (Jc[3] + Jc[2])/2;
-	
-	Oc[0] = i;
-	Oc[1] = j;
-	int width;
-	ibyte* p, *pc;
-	width=rgb.width;
+	float dmin1 = 1e9, dmin2 = 1e9;
+	int label1 = 0, label2 = 0;
+
+	for (l = 1; l <= nlabel; l++) {
+		centroid(a, label, l, ic, jc);
+		float R[8] = { 0 };
+
+		for (int d = 0; d < 8; d++) {
+			i = ic;
+			j = jc;
+			while (true) {
+				i += dx[d];
+				j += dy[d];
+				if (i < 0 || j < 0 || i >= width || j >= height) break;
+
+				k = i + j * width;
+				if (pl[k] != l) break;
+
+				float x = i - ic;
+				float y = j - jc;
+				R[d] = sqrt(x * x + y * y);
+			}
+		}
+
+		float min = R[0], max = R[0];
+		for (int d = 1; d < 8; d++) {
+			if (R[d] < min) min = R[d];
+			if (R[d] > max) max = R[d];
+		}
+
+		float diff = max - min;
+
+		if (diff > 0) {
+			if (diff < dmin1) {
+				dmin2 = dmin1;
+				label2 = label1;
+				dmin1 = diff;
+				label1 = l;
+			}
+			else if (diff < dmin2) {
+				dmin2 = diff;
+				label2 = l;
+			}
+		}
+	}
+
+	if (label1 != -1) {
+		ObsLabel[0] = label1;
+		centroid(a, label, label1, ic, jc);
+		//draw_point_rgb(rgb, (int)ic, (int)jc, 255, 0, 0);
+	}
+	if (label2 != -1) {
+		ObsLabel[1] = label2;
+		centroid(a, label, label2, ic, jc);
+		//draw_point_rgb(rgb, (int)ic, (int)jc, 255, 0, 0);
+	}
+}
+
+void opponent_track(int Ic[4], int Jc[4], image& rgb,image &label, int& pw_r, int& pw_l) {
+
+	int id = (Ic[3] + Ic[2]) / 2;
+	int jd = (Jc[3] + Jc[2]) / 2;
+
+	int width = rgb.width;
+	int height = rgb.height;
+	int size = width * height;
+
+	int k,i,j;
+	i2byte* pl, * plc;
+
+	pl = (i2byte*)label.pdata;
+
 
 	//drawing line between red point and enemy
 	//line is y=mx+b
-	
-	int dx = (Ic[0] - i);
-	int dy = (Jc[0] - j);
 
-	float m,b,y;
+	int dx = (Ic[0] - id);
+	int dy = (Jc[0] - jd);
 
-	if (dx == 0){
+	float m, b, y;
+
+	if (dx == 0) {
 		m = 0;
 		b = Ic[0];
 	}
@@ -99,10 +205,10 @@ void opponent_track(int Ic[4], int Jc[4], int Oc[2], image &rgb, int &pw_r, int 
 	//cout << "\ny= " << m << " + " << b;
 
 	//want to check if the red dot Ic[1], Jc[1] is bellow or above the line
-	float deadzone=10;
+	float deadzone = 10;
 	y = m * Ic[1] + b;
 
-	if (i <= Ic[0]) {
+	if (id <= Ic[0]) {
 		if (Jc[1] < (y - deadzone)) {
 			pw_r = 2000;
 			pw_l = 2000;
@@ -117,7 +223,7 @@ void opponent_track(int Ic[4], int Jc[4], int Oc[2], image &rgb, int &pw_r, int 
 		}
 	}
 
-	else if (i > Ic[0]) {
+	else if (id > Ic[0]) {
 		if (Jc[1] < (y - deadzone)) {
 			pw_r = 1000;
 			pw_l = 1000;
@@ -131,66 +237,121 @@ void opponent_track(int Ic[4], int Jc[4], int Oc[2], image &rgb, int &pw_r, int 
 			pw_l = 1500;
 		}
 	}
-
-	
-	
-
 
 	//drawing point
-	draw_point_rgb(rgb, i, j, 0, 0, 255);
+	draw_point_rgb(rgb, id, jd, 255, 0, 255);
 
 }
 
-void find_obstacles(image& rgb, image& label, image&a,int nlabel,int Obs[2][2]) {
-	int k, i, j, width, size, height, l, 
-		area, max_area1=0, max_area2=0, max_label1=0, max_label2=0;
-	double io1=0, jo1=0, io2=0, jo2=0;
-	ibyte* p, * pc;
-	i2byte* pl, * plc;
+void detect_obstruction(int Ic[4], int Jc[4], int ObsLabel[2], image& rgb, image& label, bool &detected) {
+	int width = rgb.width;
+	int height = rgb.height;
+	int size = width * height;
+	bool obstruction=false;
 
+	// Pointer to label data
+	i2byte* pl = (i2byte*)label.pdata;
+
+	// Center of defending
+	int cd_x = (Ic[2] + Ic[3]) / 2;
+	int cd_y = (Jc[2] + Jc[3]) / 2;
+
+	// Center of rotation attacking
+	int cr_x = Ic[0];
+	int cr_y = Jc[0];
+
+	// Calculate line equation parameters y = mx + b
+	float dx = cr_x - cd_x;
+	float dy = cr_y - cd_y;
+
+	if (dx == 0) {
+		for (int k = 0; k < size; k++) {
+			int i = k % width;
+			int j = (k - i) / width;
+			if (abs(i - (int)cr_x) <= 1) {
+				draw_point_rgb(rgb, i, j, 255, 0, 0);
+				if (pl[k] == ObsLabel[0] || pl[k] == ObsLabel[1]) obstruction = true;
+			}
+		}
+	}
+	else {
+		float m = dy / dx;
+		float b = cr_y - cr_x * m;
+		//line drawing
+
+		for (int k = 0; k < size; k++) {
+			int i = k % width;
+			int j = (k - i) / width;
+			float y = i * m + b;
+			if (abs(j - (int)y) <= 1) {
+				draw_point_rgb(rgb, i, j, 255, 0, 0);
+				if (pl[k] == ObsLabel[0] || pl[k] == ObsLabel[1]) obstruction = true;
+			}
+		}
+	}
+	detected = obstruction;
+}
+
+void go_to(int Ic[4], int Jc[4], int& pw_l, int& pw_r, image& rgb,image &label, int id,int jd) {
+	int i, j, k, width, size, height;
 	width = rgb.width;
 	height = rgb.height;
 	size = width * height;
-
-	p = rgb.pdata;
+	bool locked = false;
+	i2byte* pl;
 	pl = (i2byte*)label.pdata;
+	k = id + jd * width;
 
-	for (l = 1; l < nlabel; l++) {
-		area = 0;
-		for (k = 0; k < size; k++) {
-			plc = pl + k;
+	if (pl[k] == 0) {
+		int ir = (Ic[0] + Ic[1]) / 2;
+		int jr = (Jc[0] + Jc[1]) / 2;
 
-			if (*plc == l) {
-				area++;
+		int V[2] = { id - ir,jd - jr }; //V[0]=x, V[1]=y
+		float angle = atan2(V[1], V[0]);
+		float robot_angle = atan2(Jc[0] - Jc[1], Ic[0] - Ic[1]);
+		float angle_error = angle - robot_angle;
+
+		draw_point_rgb(rgb, id, jd, 200, 0, 0);
+
+		if (fabs(angle_error) > 0.05) {
+			if (angle_error > 0) {
+				pw_l = 2000;
+				pw_r = 2000;
+			}
+			else {
+				pw_l = 1000;
+				pw_r = 1000;
+			}
+			locked = false;
+		}
+		else {
+			pw_l = 1500;
+			pw_r = 1500;
+			locked = true;
+		}
+
+		int x_error = abs(id - Ic[0]);
+		int y_error = abs(jd - Jc[0]);
+
+		if (x_error > 10 && y_error > 10) {
+			if (locked == true) {
+				pw_l = 1000;
+				pw_r = 2000;
 			}
 		}
-		if (area > max_area1) {
-			max_area2 = max_area1;
-			max_label2 = max_label1;
-			io2 = io1;
-			jo2 = jo1;
-
-			max_area1 = area;
-			max_label1 = l;
-			centroid(a, label,l, io1, jo1);
+		else if (x_error > 2 && y_error > 2) {
+			if (locked == true) {
+				pw_l = 1250;
+				pw_r = 1750;
+			}
 		}
-		else if (area > max_area2) {
-			max_area2 = area;
-			max_label2 = 2;
-			centroid(a, label, l, io2, jo2);
+		else if (x_error > 1 && y_error < 1){
+			pw_l = 1500;
+			pw_r = 1500;
 		}
 	}
 
-	Obs[0][0] = (int)io1;
-	Obs[0][1] = (int)jo1;
-	Obs[1][0] = (int)io2;
-	Obs[1][1] = (int)jo2;
-	
-	draw_point_rgb(rgb, (int)io1, (int)jo1, 255, 0, 0);
-	draw_point_rgb(rgb, (int)io2, (int)jo2, 0, 255, 0);
-	//cout <<"\nA1 = "<< max_area1<<" l1 = "<<max_label1<<" io = "<<io1<<" jo1 = "<<jo1;
 }
-
 
 //Fred functions
 int auto_select_shape_by_size(i2byte& nlabel, image& label)
