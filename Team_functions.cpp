e27be7c17cb20e07c8d45350a00e8977baf99c96
Sankyo24 <frederiquegagne24@gmail.com>
2025-05-01 11:20:11 -0400

    Update Team_functions.cpp

--- Team_functions.cpp
+++ Team_functions.cpp
@@ -353,6 +353,8 @@ void go_to(int Ic[4], int Jc[4], int& pw_l, int& pw_r, image& rgb,image &label, 
 
 }
 
+
+
 //Fred functions
 int auto_select_shape_by_size(i2byte& nlabel, image& label)
 // select an object from a binary image based on its area
@@ -388,6 +390,285 @@ int auto_select_shape_by_size(i2byte& nlabel, image& label)
 		}
 	}
 	return 0; // no errors
+}
+
+double estimate_radius_from_image(image& rgb_obstacle, double IC, double JC) {
+	double radius, radius_max, radius_div, arc, arc_max, arc_div;
+	int i, j, width, height, k;
+	const double PI = 3.14159;
+	ibyte* pc;
+	ibyte r, g, b;
+
+	pc = rgb_obstacle.pdata;
+	radius_max = 100;
+	radius_div = 1;
+	arc_div = 1;
+	width = rgb_obstacle.width;
+	height = rgb_obstacle.height;
+
+
+	for (radius = 1; radius < radius_max; radius += radius_div) {
+		arc_max = 2 * PI * radius;
+		for (arc = 0; arc <= arc_max; arc += arc_div) {
+			double beta = arc / radius;
+			i = (int)(IC + radius * cos(beta));
+			j = (int)(JC + radius * sin(beta));
+
+			// limit (i,j) from going off the image
+			if (i < 0) i = 0;
+			if (i > width - 1) i = width - 1;
+			if (j < 0) j = 0;
+			if (j > height - 1) j = height - 1;
+
+			k = i + width * j;
+			r = pc[3 * k + 0];
+			g = pc[3 * k + 1];
+			b = pc[3 * k + 2];
+
+			if (r > 250 && g > 250 && b > 250) {
+				return radius; // obstacle ends, return radius
+			}
+			
+		}
+	}
+	return radius_max; // max radius if full disk
+}
+
+bool is_robot_in_line_of_sight(robot* defender, robot_system* S1, double x_obs[], double y_obs[], int N_obs) {
+	double defender_x = defender->x[2];
+	double defender_y = defender->x[3];
+	double opponent_x = S1->P[2]->x[2];
+	double opponent_y = S1->P[2]->x[3];
+	double dx, dy, fx, fy, obs_x, obs_y, a, b, c, sqrt_quad, t1, t2;
+	double angle_to_robot, opponent_angle, relative_angle;
+	const double PI = 3.14159265358979323846;
+
+	dx = defender_x - opponent_x;
+	dy = defender_y - opponent_y;
+	
+	// Compute relative angle between defender direction and opponent
+	angle_to_robot = atan2(dy, dx);
+	opponent_angle = defender->x[1];
+	//while (robot_angle > PI) robot_angle -= 2 * PI;
+	//while (robot_angle < -PI) robot_angle += 2 * PI;
+	relative_angle = angle_to_robot - opponent_angle;
+
+	// Normalize the angle to be between -pi and pi
+	while (relative_angle > PI) relative_angle -= 2 * PI;
+	while (relative_angle < -PI) relative_angle += 2 * PI;
+
+	//check if the opponent line of sight is obstructed by an obstacle
+	for (int i = 0; i < N_obs; i++) {
+		obs_x = x_obs[i];
+		obs_y = y_obs[i];
+		fx = obs_x - opponent_x;
+		fy = obs_y - opponent_y;
+
+		double radius = estimate_radius_from_image(rgb_obstacle[i], x_obs[i], y_obs[i]);
+
+		a = dx * dx + dy * dy;
+		b = 2 * (fx * dx + fy * dy);
+		c = fx * fx + fy * fy - radius * radius;
+
+		sqrt_quad = b * b - 4 * a * c;
+
+		if (sqrt_quad >= 0) {
+			
+			sqrt_quad = sqrt(sqrt_quad);
+			t1 = (-b - sqrt_quad) / (2 * a);
+			t2 = (-b + sqrt_quad) / (2 * a);
+			//cout << "\nt1 = " << t1 <<"  t2 = "<< t2;
+			// If the line from robot to opponent intersects this obstacle
+			if ((t1 >= 0 && t1<= 1) || (t2 >= 0 && t2 <= 1)) {
+				return false;  // Line of sight is blocked
+			}
+			
+		}
+
+	}
+	//cout << "\n angle = " << relative_angle;
+	// If no obstacle blocks the view, check if robot is in opponent's field of view
+	if (fabs(relative_angle) < (PI / 2)) { 
+		return true; // The robot is in the opponent's line of sight
+	}
+	
+	return false; // The robot is not in the opponent's line of sight
+}
+
+void find_hiding_position(robot_system* S1, double& hide_x, double& hide_y, double& hide_theta, double x_obs[], double y_obs[], int N_obs) {
+	double rx = S1->P[1]->x[2]; // defender robot's x
+	double ry = S1->P[1]->x[3]; // defender robot's y
+	double opp_x = S1->P[2]->x[2]; // opponent x
+	double opp_y = S1->P[2]->x[3]; // opponent y
+	double opp_theta = S1->P[2]->x[1]; // opponent heading
+
+	const double PI = 3.14159265358979323846;
+
+	// Check if we are already in opponent's sight
+	// If not seen, prefer to stay at current spot
+	if (!is_robot_in_line_of_sight(S1->P[1], S1, x_obs, y_obs, N_obs)) {
+		// No need to find a new hiding spot
+		return;
+	}
+
+	// Otherwise, find a better hiding spot
+	double best_score = 1e6; // <- lower score is better (NOT 1e9!)
+
+	for (int i = 0; i < N_obs; i++) {
+		double obs_x = x_obs[i];
+		double obs_y = y_obs[i];
+
+		// Direction from opponent to obstacle
+		double dx = obs_x - opp_x;
+		double dy = obs_y - opp_y;
+		double dist_to_opp_sq = dx * dx + dy * dy;
+
+		// Candidate hiding position (behind obstacle relative to opponent)
+		double candidate_x = obs_x + dx * 0.5;
+		double candidate_y = obs_y + dy * 0.5;
+
+		// Distance from defender to this hiding spot
+		double dx_robot = candidate_x - rx;
+		double dy_robot = candidate_y - ry;
+		double dist_to_robot_sq = dx_robot * dx_robot + dy_robot * dy_robot;
+
+		// Scoring function: prefer obstacles far from opponent, but not too far from us
+		double score = dist_to_opp_sq - 0.5 * dist_to_robot_sq;
+
+		if (score < best_score) {
+			best_score = score;
+			hide_x = candidate_x;
+			hide_y = candidate_y;
+		}
+	}
+
+}
+
+void navigate_to_target(robot* defender, double hide_x, double hide_y, double x_obs[], double y_obs[], int N_obs, int& pw_l, int& pw_r) {
+	double rx = defender->x[2];
+	double ry = defender->x[3];
+	double rtheta = defender->x[1];
+	const double PI = 3.14159265358979323846;
+	double dx = hide_x - rx;
+	double dy = hide_y - ry;
+	double dist = sqrt(dx * dx + dy * dy);
+	
+	// Obstacle repulsion
+	double repulse_x = 0;
+	double repulse_y = 0;
+	for (int i = 0; i < N_obs; i++) {
+		double obs_dx = rx - x_obs[i];
+		double obs_dy = ry - y_obs[i];
+		double dist_sq = obs_dx * obs_dx + obs_dy * obs_dy;
+		if (dist_sq < 10000 && dist_sq > 1.0) {
+			double force = 10000.0 / dist_sq;
+			repulse_x += force * obs_dx;
+			repulse_y += force * obs_dy;
+		}
+	}
+	
+	dx += repulse_x;
+	dy += repulse_y;
+	
+	
+	double angle_to_target = atan2(dy, dx);
+	double angle_diff = angle_to_target - rtheta;
+
+	while (angle_diff > PI) angle_diff -= 2 * PI;
+	while (angle_diff < -PI) angle_diff += 2 * PI;
+
+	
+	//if more than 80 pixels away, forward_speed = 0.5
+	//if less, forward_speed = 0.2
+	double forward_speed = (dist > 80) ? 0.5 : 0.2;
+	double turn_speed = 0.3;
+
+	
+	//if not facing target within 0.1rad (6deg), turn
+	if (angle_diff > 0.1) {
+		defender->set_inputs(1500 - forward_speed * 400, 1500 + turn_speed * 400, 1500, 0);
+	}
+	else if (angle_diff < -0.1) {
+		defender->set_inputs(1500 + turn_speed * 400, 1500 - forward_speed * 400, 1500, 0);
+	}
+	else {
+		defender->set_inputs(1500 + forward_speed * 400, 1500 + forward_speed * 400, 1500, 0);
+	}
+
+	const int pw_base = 1500;
+	const int max_delta = 500;
+	
+	int speed = (int)(dist * 5);
+	if (speed > 300) speed = 300;
+	if (dist < 5) speed = 0;
+
+	int turn = (int)(angle_diff * 400);
+	if (turn > max_delta) turn = max_delta;
+	if (turn < -max_delta) turn = -max_delta;
+	if (dist < 30 && fabs(turn) > 100) {
+		turn /= 2;
+	}
+
+
+	pw_l = pw_base - speed + turn;
+	pw_r = pw_base + speed + turn;
+
+	if (pw_l < 1000) pw_l = 1000;
+	if (pw_l > 2000) pw_l = 2000;
+	if (pw_r < 1000) pw_r = 1000;
+	if (pw_r > 2000) pw_r = 2000;
+}
+
+void dynamic_hide(robot* defender, robot_system* S1, double x_obs[], double y_obs[], int N_obs, int& pw_l, int& pw_r) {
+	static double hide_x = 0;
+	static double hide_y = 0;
+	static double hide_theta = 0;
+	static double last_opp_x = 0;
+	static double last_opp_y = 0;
+
+	double curr_opp_x = S1->P[2]->x[2];
+	double curr_opp_y = S1->P[2]->x[3];
+
+	bool exposed = is_robot_in_line_of_sight(defender, S1, x_obs, y_obs, N_obs);
+
+	double dx = curr_opp_x - last_opp_x;
+	double dy = curr_opp_y - last_opp_y;
+	double opponent_speed = sqrt(dx * dx + dy * dy);
+
+	bool opponent_moved = opponent_speed > 1.0; // Movement threshold
+	find_hiding_position(S1, hide_x, hide_y, hide_theta, x_obs, y_obs, N_obs);
+
+	if (exposed) {
+		//find_hiding_position(S1, hide_x, hide_y, hide_theta, x_obs, y_obs, N_obs);
+		// If the robot is in the line of sight, move away from the opponent's line of sight
+		double defender_x = defender->x[2];
+		double defender_y = defender->x[3];
+		double opponent_x = S1->P[2]->x[2];
+		double opponent_y = S1->P[2]->x[3];
+
+		// Perpendicular direction calculation to avoid LoS
+		double dx = opponent_x - defender_x;
+		double dy = opponent_y - defender_y;
+
+		double perpendicular_dx = -dy; // Rotate 90 degrees to the left
+		double perpendicular_dy = dx; // Rotate 90 degrees to the left
+
+		// Normalize the perpendicular direction
+		double magnitude = sqrt(perpendicular_dx * perpendicular_dx + perpendicular_dy * perpendicular_dy);
+		perpendicular_dx /= magnitude;
+		perpendicular_dy /= magnitude;
+
+		// Calculate the new target position based on the perpendicular vector
+		double move_x = defender_x + perpendicular_dx * 100; // Move by 100 units in the perpendicular direction
+		double move_y = defender_y + perpendicular_dy * 100;
+
+		// Use the existing navigation function to move the robot
+		navigate_to_target(defender, hide_x, hide_y, x_obs, y_obs, N_obs, pw_l, pw_r);
+	}
+	find_hiding_position(S1, hide_x, hide_y, hide_theta, x_obs, y_obs, N_obs);
+	//navigate_to_target(defender, hide_x, hide_y, x_obs, y_obs, N_obs, pw_l, pw_r);
+	last_opp_x = curr_opp_x;
+	last_opp_y = curr_opp_y;
 }
 
 //marc functions
